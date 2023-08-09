5968fd58dbd626e641f3ef56521ed674de158250
Dan <dtwalter@gmail.com>
2023-08-09 00:17:26 -0400

    Update hint7.py

--- hints/hint7.py
+++ hints/hint7.py
@@ -1,5 +1,5 @@
 '''
-6 is the correct first digit
+1 is the correct first digit
 
 Group 1's pin guesses are all less than the correct guess
 
@@ -8,5 +8,5 @@ Group 2's pin guesses are all greater than the correct pin
 We might assume that this pattern will hold for future pin digits, which would mean character
 guesses lower than the correct digit will result in a dipping trace while character guesses
 higher than the correct digit will result in a rising trace. This might suggest that the second
-digit is higher than 0, since the trace for 6000 dips after 940 similar to group 1 earlier.
+digit is higher than 0, since the trace for 1000 dips after 940 similar to group 1 earlier.
 '''
